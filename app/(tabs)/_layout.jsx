d48bd5b35398f25645d868067d704d34305b2276
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-06-24 23:47:44 +0100

    add icons for tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,15 +1,36 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { Octicons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 const TabsLayout = () => {
   return (
-    <Tabs
-    screenOptions={{headerShown: false}}
-    >
-      <Tabs.Screen name="Home" />
-      <Tabs.Screen name="Explore" />
-      <Tabs.Screen name="Profile" />
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="Home"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: () => <Octicons name="home" size={24} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="Explore"
+        options={{
+          tabBarLabel: "Explore",
+          tabBarIcon: () => <Feather name="search" size={24} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: () => (
+            <FontAwesome6 name="user-circle" size={24} color="black" />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
